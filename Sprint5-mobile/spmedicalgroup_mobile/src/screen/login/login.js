823fe82638085c1f2f442587e084f2c6781104a0
52645548890 <52645548890@DESKTOP-9K54HD5>
2021-12-03 17:13:14 -0300

    login feita

--- Sprint5-mobile/spmedicalgroup_mobile/src/screen/login/login.js
+++ Sprint5-mobile/spmedicalgroup_mobile/src/screen/login/login.js
@@ -3,51 +3,73 @@ import React, { useState } from 'react'
 
 //Services
 import api from '../../services/api';
-import { useAsyncStorage } from '@react-native-async-storage/async-storage';
+
+//Components Importados
+import  AsyncStorage  from '@react-native-async-storage/async-storage';
 import { useNavigation } from '@react-navigation/core';
+import jwtDecode from 'jwt-decode';
 
 //Components React Native
-import { View, StyleSheet, Image, Text } from 'react-native';
+import { View, StyleSheet, Image, Text, TextInput, TouchableOpacity, Alert } from 'react-native';
 
 
 export default function Login() {
 
     //Hooks
-    const asyncStorage = useAsyncStorage();
     const navigation = useNavigation();
 
     //States
-    const [email, setEmail] = useState();
-    const [senha, setSenha] = useState();
+    const [email, setEmail] = useState('ricardo.lemos@spmedicalgroup.com.br');
+    const [senha, setSenha] = useState('12345678');
 
-    function Logar() {
-
-        try {
+        async function Logar () {
             //Requisição 
-            const resposta = api.post('/Login/login', {
+          const resposta = await api.post('/Login/login', {
                 email: email,
                 senha: senha
             })
 
-            //setando token
             const token = resposta.data.token;
 
-            // Colocando token no localStorage do celular
-            asyncStorage.setItem('userToken', token);
+            await AsyncStorage.setItem('userToken', token);
+
+            const userRole = jwtDecode(token).role;
 
             if (resposta.status === 200) {
-                navigation.navigate("Main")
+                if(userRole === 2){
+                    navigation.navigate("Main")
+                }
+                else if(userRole === 1){
+                    navigation.navigate("Main")
+                } else {
+                    Alert.alert('403');
+                }
             }
-        } catch (error) {
-            console.warn(error)
-        }
+
+            
+        
     }
 
     return (
-        <View styles={styles.container}>
-            <View styles={styles.logoBox}>
+        <View style={styles.container}>
+
+
+            <View style={styles.logoBox}>
                 <Image source={require("../../../assets/img/logo_login.png")} />
                 <Text style={styles.logoText}>SpMedicalGroup</Text>
+            </View>
+
+            <View style={styles.loginFormBox}>
+                <TextInput onChangeText={email => setEmail(email)} style={styles.loginFormBoxInput} placeholsder="Email" />
+                <TextInput onChangeText={senha => setSenha(senha)} style={styles.loginFormBoxInput} placeholder="Senha" />
+
+                <TouchableOpacity onPress={Logar} style={styles.loginBotaoCadastrar}>
+                    <Text style={styles.loginBotaoCadastrarText}>Entrar</Text>
+                </TouchableOpacity>
+            </View>
+
+            <View style={styles.loginFooter}>
+                <Text style={styles.loginFooterText}>© 2021 SpMedicalGroup. Todos os direitos reservados.</Text>
             </View>
         </View>
     )
@@ -56,17 +78,63 @@ export default function Login() {
 const styles = StyleSheet.create(
     {
         container: {
-            flex: 1
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+            flexDirection: 'column'
         },
 
         logoBox: {
-            flex: 1,
-            flexDirection: 'column'
+            alignItems: 'center',
+            marginBottom: 20
         },
 
         logoText: {
             fontFamily: "TenorSans-Regular",
-            fontSize: 27
+            fontSize: 27,
+            color: 'black'
+        },
+
+        loginFormBox: {
+            alignItems: 'center'
+        },
+
+        loginFormBoxInput: {
+            width: 306,
+            paddingLeft: 5,
+            paddingBottom: 2,
+            marginBottom: 10,
+            fontSize: 19,
+            borderBottomWidth: 1,
+            borderBottomColor: '#025B5E',
+            fontFamily: 'Barlow-Regular',
+        },
+
+        loginBotaoCadastrar: {
+            width: 247,
+            height: 42,
+            marginTop: 35,
+            backgroundColor: '#025B5E',
+            alignItems: 'center',
+            justifyContent: 'center',
+            borderRadius: 20
+        },
+
+        loginBotaoCadastrarText: {
+            color: 'white',
+            fontSize: 23,
+            fontFamily: 'TitilliumWeb-Bold'
+        },
+
+        loginFooter: {
+            position: 'absolute',
+            bottom: 35
+        },
+
+        loginFooterText: {
+            fontFamily: 'TitilliumWeb-Bold',
+            fontSize: 10,
+            color: 'black'
         }
     }
 );
